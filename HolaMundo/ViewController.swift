d5127c4f5cf2a339ee61b471be389f55fe811cc4
Fernando Negrete Pimentel - vn5451i <fnegrete9510@gmail.com>
2023-05-17 11:01:31 -0600

    Create proyect

---
+++ HolaMundo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by Fernando Negrete Pimentel on 16/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
